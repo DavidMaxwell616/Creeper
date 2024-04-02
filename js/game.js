8e8a8a90bbc52d5a24042d73904214e5a62605de
David Maxwell <david.w.maxwell@gmail.com>
2024-04-01 22:54:24 -0700

    update

--- js/game.js
+++ js/game.js
@@ -13,63 +13,53 @@ var config = {
 var game = new Phaser.Game(config);
 function create() {
   background = this.add.image(400, 300, 'bkgd').setScale(2);
-  spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
   dude = this.add.sprite(592, 249, 'dude').setScale(2);
+  girlHead = this.add.sprite(288, 111, 'girl head').setScale(2);
+  girlTurn = this.add.sprite(288, 231, 'girlTurns').setScale(2);
+  girlTurn.visible = false;
   this.anims.create({
-    key: 'dudeChillin',
-    frames: this.anims.generateFrameNumbers('dude',
-      {
-        start: 32,
-        end: 32
-      }),
-    frameRate: 16,
-    repeat: -1,
-    yoyo: true
-  })
-
-  this.anims.create({
-    key: 'dudeLookin',
-    frames: this.anims.generateFrameNumbers('dude',
-      {
-        start: 32,
-        end: 14
-      }),
-    frameRate: 16,
-  })
-  this.anims.create({
-    key: 'dudeLookinAway',
+    key: 'dudeBusted',
     frames: this.anims.generateFrameNumbers('dude',
       {
         start: 0,
         end: 5
       }),
     frameRate: 16,
-    repeat: -1,
-    yoyo: true
-  })
+    repeat: 0
+  });
+  canCrush = this.add.sprite(606, 300, 'canCrush').setScale(2);
   this.anims.create({
-    key: 'dudeCaught',
-    frames: this.anims.generateFrameNumbers('dude',
+    key: 'canCrushDude',
+    frames: this.anims.generateFrameNumbers('canCrush',
       {
-        start: 6,
+        start: 0,
         end: 12
       }),
     frameRate: 16,
-    repeat: -1,
-    yoyo: true
+    repeat: 0
   });
-
-  girlHead = this.add.sprite(288, 111, 'girl head').setScale(2);
+  canCrush.visible = false;
   this.anims.create({
-    key: 'girlChillin',
-    frames: this.anims.generateFrameNumbers('girl head', {}),
-    frameRate: 10,
+    key: 'girlTurning',
+    frames: this.anims.generateFrameNumbers('girlTurns',
+      {
+        start: 0,
+        end: 19
+      }),
+    frameRate: 16,
     repeat: 0
   });
 
-  dude.anims.play('dudeChillin');
-  //girlHead.anims.play('girlChillin');
-
+  this.anims.create({
+    key: 'dudeWon',
+    frames: this.anims.generateFrameNumbers('dude',
+      {
+        start: 33,
+        end: 43
+      }),
+    frameRate: 16,
+    repeat: 0
+  });
   this.add.text(40, 50, 'OGLE-METER', {
     fontFamily: 'Stencil',
     fontSize: '24px',
@@ -99,9 +89,9 @@ function create() {
     .on('pointerover', () => startButton.setStyle({ fill: '#B38C40' }))
     .on('pointerout', () => startButton.setStyle({ fill: '#ffffff' }))
     .setOrigin(.5);
-  const slider = this.add.container(game.config.width/2,game.config.height-50);
-  ogleSlide = this.add.image(0,0, 'ogleslide');
-  ogleSlider = this.add.image(ogleSlide.width/2,0, 'ogleslider');
+  slider = this.add.container(game.config.width / 2, game.config.height - 50);
+  ogleSlide = this.add.image(0, 0, 'ogleslide');
+  ogleSlider = this.add.image(ogleSlide.width / 2, 0, 'ogleslider');
   slider.add([ogleSlide, ogleSlider]);
   ogleSlider.setInteractive({ draggable: true });
   ogleSlider.on('drag', function (pointer, dragX, dragY) {
@@ -112,25 +102,131 @@ function create() {
   slider.on('drag', function (pointer, dragX, dragY) {
     slider.x = dragX;
   });
-
-
-}
+  slider.visible = false;
+  meterValue = this.add.sprite(16, 30, 'meterValue');
+  meterValue.setOrigin(0, 0.5);
+  wonText = this.add.text(game.config.width / 2, game.config.height / 2, 'YOU WON!!', {
+    fontFamily: 'Stencil',
+    fontSize: '48px',
+    color: '#ffffff',
+    shadow: { offsetX: 2, offsetY: 2, color: '#000000', fill: true }
+  });
+  wonText.setOrigin(0.5);
+  wonText.visible = false;
+  playAgainButton = this.add.text(game.config.width / 2, game.config.height / 2 + 80, 'PLAY AGAIN?', {
+    fontFamily: 'Stencil',
+    fontSize: '48px',
+    color: '#ffffff',
+    shadow: { offsetX: 2, offsetY: 2, color: '#000000', fill: true }
+  });
+  playAgainButton.setInteractive()
+    .on('pointerdown', () => ResetGame())
+    .on('pointerover', () => playAgainButton.setStyle({ fill: '#B38C40' }))
+    .on('pointerout', () => playAgainButton.setStyle({ fill: '#ffffff' }))
+    .setOrigin(.5);
+  playAgainButton.visible = false;
+  timesUpText = this.add.text(game.config.width / 2, game.config.height / 2, "TIME'S UP", {
+    fontFamily: 'Stencil',
+    fontSize: '48px',
+    color: '#ffffff',
+    shadow: { offsetX: 2, offsetY: 2, color: '#000000', fill: true }
+  });
+  timesUpText.setOrigin(0.5);
+  timesUpText.visible = false;
+  bustedText = this.add.text(game.config.width / 2, game.config.height / 2, "BUSTED!", {
+    fontFamily: 'Stencil',
+    fontSize: '48px',
+    color: '#ffffff',
+    shadow: { offsetX: 2, offsetY: 2, color: '#000000', fill: true }
+  });
+  bustedText.setOrigin(0.5);
+  bustedText.visible = false;
+  music = this.sound.add('music');
+  hey = this.sound.add('hey');
+  timesUp = this.sound.add('timesUp');
+  uhOh = this.sound.add('uhOh');
+  busted = this.sound.add('busted');
+  yay = this.sound.add('yay');
+  tenSeconds = this.sound.add('tenSeconds');
+};
 
 function StartGame() {
   startButton.visible = false;
+  slider.visible = true;
   startGame = true;
-}
+  music.play();
+  music.setLoop(true);
+};
 
-function DudeLooks() {
-  isLooking = true;
-  isLookingAway = false;
-  dude.anims.play('dudeLookin');
-}
-function DudeLooksAway() {
+function ResetGame() {
+  playAgainButton.visible = false;
+  wonText.visible = false;
+  ogleValue = 0;
+  dude.setFrame(32);
+  slider.visible = true;
+  ogleSlider.x = 250;
+  timerValue = 60;
+  startGame = true;
   isLooking = false;
-  isLookingAway = true;
-  dude.anims.play('dudeLookinAway');
-}
+  canCrush.visible = false;
+  meterValue.scaleX = 1;
+  girlSuspiciousTimer=0;
+  girlTurn.visible = false;
+  girlHead.visible = true;
+  timesUpText.visible = false;
+};
+
+function TimesUp() {
+  startGame = false;
+  gameOver = true;
+  slider.visible = false;
+  timesUp.play();
+  playAgainButton.visible = true;
+  timesUpText.visible = true;
+};
+
+function Busted() {
+  canCrush.visible = true;
+  startGame = false;
+  gameOver = true;
+  slider.visible = false;
+  timesUp.play();
+  playAgainButton.visible = true;
+  bustedText.visible = true;
+  canCrush.anims.play('canCrushDude');
+  busted.play();
+};
+
+function girlChilling() {
+  girlHead.setFrame(Math.round(girlHeadPosition));
+  if (++girlHeadGazeTimer > girlGazeLength && girlGazing) {
+    girlHeadNextPosition = Phaser.Math.Between(1, 26);
+    girlGazeLength = Phaser.Math.Between(50, 400);
+    girlHeadGazeTimer = 0;
+    girlGazing = false;
+    girlScanning = true;
+  }
+  if (girlScanning && girlHeadPosition < girlHeadNextPosition)
+    girlHeadPosition += .25;
+  else if (girlScanning && girlHeadPosition > girlHeadNextPosition)
+    girlHeadPosition -= .25;
+  if (girlScanning && girlHeadPosition == girlHeadNextPosition) {
+    girlHeadPosition = girlHeadNextPosition;
+    girlGazing = true;
+    girlScanning = false;
+  }
+};
+
+function dudeWon() {
+  startGame = false;
+  gameOver = true;
+  dude.anims.play('dudeWon');
+  yay.play();
+  slider.visible = false;
+  wonText.visible = true;
+  playAgainButton.visible = true;
+};
+
 function update() {
   if (!startGame)
     return;
@@ -141,15 +237,37 @@ function update() {
     timerText.setText(timerValue + ' SEC');
   }
   if (timerValue == 0) {
-    startGame = false;
-    gameOver = true;
+    TimesUp();
   }
-  if (spaceBar.isDown && !isLooking) {
-    DudeLooks();
+  var dudeFrame = Math.round((ogleSlider.x + ogleSlide.width / 2) / 17.8) + 7;
+  dude.setFrame(dudeFrame);
+  isLooking = dudeFrame < 12;
+  girlSuspicious = isLooking && girlHeadPosition > 10;
+  if (!girlSuspicious)
+    girlChilling();
+  else {
+    girlGazing = false;
+    girlScanning = false;
+
+    if (++girlSuspiciousTimer > 10) {
+      console.log(girlSuspicious);
+      gameOver = true;
+      girlHead.visible = false;
+      girlTurn.visible = true;
+      girlTurn.anims.play('girlTurning');
+      hey.play();
+      slider.visible = false;
+      dude.anims.play('dudeBusted');
+      uhOh.play();
+      dude.on('animationcomplete', Busted);
+    }
   }
-  if (spaceBar.isUp) {
-    DudeLooksAway();
+  if (isLooking && meterValue.scaleX < 198) {
+    ogleValue += (12 - dudeFrame) / 100;
+    meterValue.scaleX += (Math.round(ogleValue) / 100);
   }
+  if (isLooking && meterValue.scaleX >= 198)
+    dudeWon();
 }
 
 
--- js/preload.js
+++ js/preload.js
@@ -2,10 +2,21 @@ function preload() {
 this.load.path = '../assets/';
 this.load.image('bkgd', 'images/background.jpg');
 this.load.image('meter', 'images/ogle meter.png');
+this.load.image('meterValue', 'images/ogle meter value.png');
 this.load.image('timer', 'images/timer.png');
 this.load.image('ogleslide', 'images/ogle slide.png');
 this.load.image('ogleslider', 'images/ogle slider.png');
 
 this.load.spritesheet('dude', 'spritesheets/dude.png', { frameWidth: 168, frameHeight: 238 });
 this.load.spritesheet('girl head', 'spritesheets/girl head.png', { frameWidth: 64, frameHeight:87 });
+this.load.spritesheet('girlTurns', 'spritesheets/girl turning.png', { frameWidth: 138, frameHeight:208 });
+this.load.spritesheet('canCrush', 'spritesheets/can crush.png', { frameWidth: 183, frameHeight:317 });
+
+this.load.audio('music', 'sounds/music.mp3');
+this.load.audio('hey', 'sounds/hey.mp3');
+this.load.audio('timesUp', 'sounds/out of time.mp3');
+this.load.audio('uhOh', 'sounds/uh oh.mp3');
+this.load.audio('busted', 'sounds/busted.mp3');
+this.load.audio('yay', 'sounds/yay.mp3');
+this.load.audio('tenSeconds', 'sounds/10 second warning.mp3');
 }
